62f5f7a36c0314a55ec3b0ad276fb3f637eb911b
Park min kyu <qkralsrb5235@naver.com>
2023-05-16 14:09:27 +0900

    0516Practice

--- src/chap01/TriangleLB.java
+++ src/chap01/TriangleLB.java
@@ -1,8 +1,25 @@
 package chap01;
 
+import java.util.Scanner;
+
 public class TriangleLB {
 
 	public static void main(String[] args) {
+		Scanner stdIn=new Scanner(System.in);
+		int n;
+		System.out.println("직각 이등변 삼각형 출력");
+		
+		do {
+			System.out.println("몇 단 삼각형입니까?");
+			n=stdIn.nextInt();
+			for(int i=1;i<=n;i++) {
+				for(int j=1;j<=i;j++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			
+		}while(n<=0);
 	}
 
 }
