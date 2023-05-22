2eb258ed533e72f02320663656cb1df633527784
Park min kyu <qkralsrb5235@naver.com>
2023-05-22 23:57:17 +0900

    배열의 생성과 초기화

---
+++ src/Array/loop_2.java
@@ -0,0 +1,18 @@
+package Array;
+
+import java.util.Arrays;
+
+public class loop_2 {
+
+	public static void main(String[] args) {
+		int[]students= {1,2,3,4,5,6,7,8,9,10};
+		System.out.println("변경 전: "+Arrays.toString(students));
+		
+		int[]students2=new int[students.length];
+		for(int i=0;i<students.length;i++) {
+			students2[(students.length-1)-i]=students[i];
+		}
+		System.out.println("변경 후: "+Arrays.toString(students2));
+	}
+
+}
