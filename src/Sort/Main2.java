ac1d74209b318f30cafbd38b1b09be6a37e74413
Park min kyu <qkralsrb5235@naver.com>
2023-05-28 00:21:40 +0900

    선택 정렬

---
+++ src/Sort/Main2.java
@@ -0,0 +1,14 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		SelectionSort selectionSort =new SelectionSort();
+		int[]arr=new int[] {2,8,0,5,7};
+		selectionSort.sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
