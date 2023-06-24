ae054898a76c8d911224721f9f2869556ae99307
Park min kyu <qkralsrb5235@naver.com>
2023-06-25 00:49:12 +0900

    최솟값 만드는 괄호찾기

---
+++ src/BackJun/P1541_최솟값을만드는괄호배치찾기.java
@@ -0,0 +1,27 @@
+package BackJun;
+
+import java.util.Scanner;
+
+public class P1541_최솟값을만드는괄호배치찾기 {
+	static int answer=0;
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String example=sc.nextLine();
+		String[]str=example.split("-");
+		for(int i=0;i<str.length;i++) {
+			int temp=mySum(str[i]);
+			if(i==0)answer=answer+temp;
+			else answer=answer-temp;
+		}
+		System.out.println(answer);
+	}
+	private static int mySum(String a) {
+		int sum=0;
+		String[]temp=a.split("[+]");
+		for(int i=0;i<temp.length;i++) {
+			sum=sum+Integer.parseInt(temp[i]);
+		}
+		return sum;
+	}
+
+}
