0ce419db75de2af8123719a5372469ff3d33aebb
Park min kyu <qkralsrb5235@naver.com>
2023-06-27 00:08:46 +0900

    점화식 활용

---
+++ src/BackJun/P_11050이항계수구하기.java
@@ -0,0 +1,28 @@
+package BackJun;
+
+import java.util.Scanner;
+
+public class P_11050이항계수구하기 {
+	static int N,K;
+	static int D[][];
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextInt();
+		K=sc.nextInt();
+		D=new int[N+1][N+1];
+		//초기화
+		for(int i=0;i<=N;i++) {
+			D[i][i]=1;
+			D[i][0]=1;
+			D[i][i]=1;
+		}
+		//점화식으로 배열 완성하기
+		for(int i=2;i<=N;i++) {
+			for(int j=1;j<=i;j++) {
+				D[i][j]=D[i-1][j]+D[i-1][j-1];
+				}
+		}
+		System.out.println(D[N][K]);
+	}
+
+}
