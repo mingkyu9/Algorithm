566c4e5d39e2811bb17ba3ac898d530532726978
Park min kyu <qkralsrb5235@naver.com>
2023-07-08 16:22:14 +0900

    구구단

---
+++ src/ITacademic/DoubleForTest01.java
@@ -0,0 +1,21 @@
+package ITacademic;
+
+public class DoubleForTest01 {
+
+	public static void main(String[] args) {
+		/*
+		 * int gugu=2; for(int i=1;i<10;i++) { System.out.println(gugu
+		 * +"*"+i+"="+(gugu*i));
+		 * 
+		 * }
+		 */
+	
+		for(int i=1;i<10;i++) {
+			for(int j=2;j<10;j++) {
+				System.out.print(j +"*"+i+"="+(i*j)+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
